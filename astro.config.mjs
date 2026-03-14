f075f759178abcf217bb0ed6073280ee78b730a1
Facundo Uferer <juanfacundouf@gmail.com>
2026-03-14 07:41:44 -0300

    feat: Update Astro base path to root and footer contact link to an external URL.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://facundouferer.github.io',
-  base: '/gotobrazil',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
